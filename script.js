bb8acbbfdcf7e6fa3ebbc768213ad983d6210008
RobinsonNguyen <robinson_nguyen@hotmail.com>
2022-12-15 08:06:14 +0900

    working checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Your JS goes here
+const container = document.getElementsByTagName('body');
+function makeSquare(color){
+    let div = document.createElement('div');
+    div.className = 'square';
+    div.style.width = '11.1%';
+    div.style.paddingBottom = '11.1%';
+    div.style.background = color;
+    div.style.float = 'left';
+    container[0].appendChild(div);
+}
+for(let i = 0; i < 81; i++){
+    if(i % 2 === 0)
+        makeSquare('black');
+    else
+        makeSquare('red');
+}
